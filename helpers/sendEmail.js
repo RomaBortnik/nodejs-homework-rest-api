b7bdc540b4d112c093c0658176a88e24ccc6408c
RomaBortnik <bortnik25051998@gmail.com>
2023-08-19 11:24:54 +0300

    added nodemailer

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,14 +1,43 @@
-const sgMail = require("@sendgrid/mail");
-require("dotenv").config();
+const nodemailer = require("nodemailer");
+const { META_PASSWORD } = process.env;
 
-const { SENDGRID_API_KEY } = process.env;
+const sendEmail = async ({ to, subject, html }) => {
+  const email = {
+    from: "bortnik25051998@meta.ua",
+    to,
+    subject,
+    html,
+  };
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+  const nodeMailerConfig = {
+    host: "smtp.meta.ua",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "bortnik25051998@meta.ua",
+      pass: META_PASSWORD,
+    },
+  };
 
-const sendEmail = async (data) => {
-  const email = { ...data, from: "bortnik25051998@gmail.com" };
-  await sgMail.send(email);
-  return true;
+  const transport = nodemailer.createTransport(nodeMailerConfig);
+
+  console.log("email", email);
+  await transport.sendMail(email);
 };
 
 module.exports = sendEmail;
+
+// const sgMail = require("@sendgrid/mail");
+// require("dotenv").config();
+
+// const { SENDGRID_API_KEY } = process.env;
+
+// sgMail.setApiKey(SENDGRID_API_KEY);
+
+// const sendEmail = async (data) => {
+//   const email = { ...data, from: "bortnik25051998@gmail.com" };
+//   await sgMail.send(email);
+//   return true;
+// };
+
+// module.exports = sendEmail;
